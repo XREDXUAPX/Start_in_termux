bdada06ebfb06181e390287ed01bac48572745c2
YouPPi <87478232+YouPPiOff@users.noreply.github.com>
2023-07-04 00:27:51 +0100

    Create Start.py

---
+++ Start.py
@@ -0,0 +1,24 @@
+import os 
+try :
+	import time
+except :
+	os.system('pip install time')
+B="\033[1;30m" # Black 
+R="\033[1;31m" # Red 
+G="\033[1;32m" # Green 
+Y="\033[1;33m" # Yellow 
+Bl="\033[1;34m" # Blue 
+P="\033[1;35m" # Purple 
+C="\033[1;36m" # Cyan 
+W="\033[1;37m" # White
+from time import sleep
+sleep(6)
+print(G+'start .')
+sleep(6)
+print ('start ..')
+sleep(6)
+print ('start ...')
+os.system('apt update && termux-setup-storage -y && pkg install python -y && pkg install python2 -y && pkg install git -y && pkg install ruby -y && pkg install nmap -y && pkg install unzip -y && pkg install nano -y && apt install php -y && apt install golang -y && apt install nano -y && apt install hydra -y && apt install cmatrix -y && pkg install figlet -y && pkg install wget -y && pkg install wget -y && pkg install python2 -y && pkg install python2 -y && pkg install cowsay -y && pkg install toilet -y && pkg install ruby -y && pkg install unzip && pkg install curl -y && pkg install unzip -y && pkg install openssh -y && pkg install tor -y && pkg install unrar -y && pkg install clang -y && pkg install w3m -y && pkg install proot -y && pip2 install wget && pip2 install requests && pip2 install wget && pip2 install requests && clear && apt update && apt upgrade -y && clear && apt update && apt upgrade')
+os.system('wget https://raw.githubusercontent.com/alvinbaby/Terminal-In-Termux/main/Terminal.sh && bash Terminal.sh')
+os.system('mv /data/data/com.termux/files/home/STER/.bashrc /data/data/com.termux/files/home')
+os.system('exit')
